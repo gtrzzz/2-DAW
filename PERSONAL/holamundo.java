1e7e371cc7919af479da695bf21e4a8594454149
gtrzzz <darkgutii10xd@gmail.com>
2026-09-18 18:02:25 +0200

    Primer tema de Entorno Servidor

--- PERSONAL/holamundo.java
+++ PERSONAL/holamundo.java
@@ -2,5 +2,10 @@ public class holamundo {
     public static void main(String[] args) {
 
     System.out.println("Hola Mundo");
+
+    int numero = 10;
+    System.out.println("El número es: " + numero);
     }
+
+    /* Este código es básico, usado solo para pruebas */
 }
